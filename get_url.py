64dd221f8ba8895fbb0f7a63df6ddc792cba5ca0
budiryan <budiryan@github.com>
2017-07-04 17:39:53 +0800

    delete unnecessary printing

--- get_url.py
+++ get_url.py
@@ -10,7 +10,6 @@ YOUR_PASSWORD = 'budihaha12345'
 if __name__ == '__main__':
     example_queries = sys.argv[1:]
     example_queries = [q.replace("_", " ") for q in example_queries]
-    print(example_queries)
     pinterest_scraper = Pinterest(YOUR_EMAIL_ADDRESS, YOUR_PASSWORD)
     # Get the urls
     urls = pinterest_scraper.get_urls(example_queries)
